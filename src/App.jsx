60c678cadce2ba127d28ae279b341b20f37e26c8
KAYSILVHA <kaysilvha@gmail.com>
2023-11-13 20:27:15 -0300

    projeto SASS

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import './Style.scss'
+
+function App() {
+  return (
+    <>
+      <header className>
+        <nav>
+          <ul>
+            <li>HOME</li>
+            <li>MOVIES</li>
+            <li>TRAILERS</li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  )
+}
+
+export default App
